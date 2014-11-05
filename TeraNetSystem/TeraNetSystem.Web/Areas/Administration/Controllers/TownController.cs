dbf6b38c1df6ed3e2c797b780dfa20d52a275817
sttodorov <st.todorov9@gmail.com>
2014-11-05 13:48:55 +0200

    Fix styles. Add edit and delete towns

---
+++ TeraNetSystem/TeraNetSystem.Web/Areas/Administration/Controllers/TownController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TeraNetSystem.Web.Controllers;
+using TeraNetSystem.Web.Areas.Administration.Models;
+using TeraNetSystem.Models;
+using System.Net;
+
+namespace TeraNetSystem.Web.Areas.Administration.Controllers
+{
+    public class TownController : BaseController
+    {
+
+        // GET: Administration/Town
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ListTowns()
+        {
+            var towns = this.Data.Towns.All().Select(TownViewModel.FromTown).ToList();
+            return View(towns);
+        }
+
+        [HttpGet]
+        public ActionResult CreatePage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(string newTownName)
+        {
+            if (ModelState.IsValid)
+            {
+                var newTown = new Town()
+                {
+                    TownName = newTownName
+                };
+
+                this.Data.Towns.Add(newTown);
+                this.Data.SaveChanges();
+            }
+
+            return RedirectToAction("ListTowns");
+        }
+
+        [HttpGet]
+        public ActionResult DeletePage(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var selectedTown = this.Data.Towns.All().Select(TownViewModel.FromTown).FirstOrDefault(t => t.Id == id);
+
+            if (selectedTown == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(selectedTown);
+
+        }
+
+
+        [HttpPost]
+        public ActionResult DeletePage(int id)
+        {
+            var townToBeDeleted = this.Data.Towns.All().FirstOrDefault(t => t.Id == id);
+
+            foreach (var office in townToBeDeleted.Offices)
+            {
+                this.Data.Offices.Delete(office);
+            }
+            foreach (var client in townToBeDeleted.Users)
+            {
+                this.Data.Users.Delete(client);
+            }
+
+            this.Data.Towns.Delete(townToBeDeleted);
+            this.Data.SaveChanges();
+
+            return RedirectToAction("ListTowns");
+        }
+
+        [HttpGet]
+        public ActionResult EditPage(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var selectedTown = this.Data.Towns.All().Select(TownViewModel.FromTown).FirstOrDefault(t => t.Id == id);
+
+            if (selectedTown == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(selectedTown);
+        }
+
+        [HttpPost]
+        public ActionResult EditPage(TownViewModel edditedTown)
+        {
+            var townToBeEdiited = this.Data.Towns.All().FirstOrDefault(t => t.Id == edditedTown.Id);
+
+            townToBeEdiited.TownName = edditedTown.TownName;
+            this.Data.SaveChanges();
+
+            return RedirectToAction("ListTowns");           
+        }
+
+    }
+}
---
+++ TeraNetSystem/TeraNetSystem.Web/Areas/Administration/Models/TownViewModel.cs
@@ -0,0 +1,35 @@
+﻿namespace TeraNetSystem.Web.Areas.Administration.Models
+{
+    using System;
+    using System.ComponentModel;
+    using System.Linq.Expressions;
+    using TeraNetSystem.Models;
+
+    public class TownViewModel
+    {
+        public static Expression<Func<Town, TownViewModel>> FromTown
+        {
+            get
+            {
+                return t => new TownViewModel
+                {
+                    Id = t.Id,
+                    TownName = t.TownName,
+                    OfficesCount = t.Offices.Count,
+                    UsersCount = t.Users.Count
+                };
+            }
+        }
+
+        public int Id { get; set; }
+
+        [DisplayName("Town name:")]
+        public string TownName { get; set; }
+
+        [DisplayName("Offices count:")]
+        public int OfficesCount { get; set; }
+
+        [DisplayName("Users count:")]
+        public int UsersCount { get; set; }
+    }
+}
