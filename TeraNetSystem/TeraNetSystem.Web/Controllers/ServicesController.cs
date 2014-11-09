cd819956535eb5dc9472b5b55a9337cdd71cbc49
sttodorov <st.todorov9@gmail.com>
2014-11-10 00:59:39 +0200

    Added requests.

---
+++ TeraNetSystem/TeraNetSystem.Web/Controllers/ServicesController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TeraNetSystem.Data;
+using TeraNetSystem.Models;
+using TeraNetSystem.Web.Areas.Administration.Models;
+using TeraNetSystem.Web.Models;
+
+namespace TeraNetSystem.Web.Controllers
+{
+    public class ServicesController : BaseController
+    {
+        public ServicesController(ITeraNetData data)
+            : base(data)
+        {
+        }
+
+        [HttpGet]
+        public ActionResult All()
+        {
+            var services = this.Data.Subscriptions.All().Select(SubscriptionViewModel.FromSubscription);
+
+            return View(services);
+        }
+
+        [HttpGet]
+        public ActionResult Request(int? id)
+        {
+            var towns = this.Data.Towns.All().Select(TownCreateModel.FromOffice).ToList();
+            var serviceId = id.GetValueOrDefault(1);
+
+            var selectedList = new List<SelectListItem>();
+
+            foreach (var item in towns)
+            {
+                selectedList.Add(new SelectListItem() { Value = item.Id.ToString(), Text = item.Name });
+            }
+
+            var createModel = new ServiceRequestModel() { Towns = selectedList, SubscriptionId = serviceId };
+            return View(createModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Request(ServiceRequestModel request)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                var createdRequest = new Request()
+                {
+                    TownId = request.TownId,
+                    Address = request.Address,
+                    FirstName = request.FirstName,
+                    LastName = request.LastName,
+                    SubscriptionId = request.SubscriptionId,
+                    PhoneNumber = request.Phone
+                };
+
+                this.Data.Requests.Add(createdRequest);
+                this.Data.SaveChanges();
+
+                TempData["Success"] = String.Format("Request for {0} subscription plan has been successfully send! We will call you in few days!",
+                                                    this.Data.Subscriptions.All().FirstOrDefault(s=>s.Id==createdRequest.SubscriptionId).SubscriptionName);                
+            }
+
+            return RedirectToAction("All");
+        }
+    }
+}
---
+++ TeraNetSystem/TeraNetSystem.Web/Models/ServiceRequestModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TeraNetSystem.Web.Models
+{
+    public class ServiceRequestModel
+    {
+        [Required]
+        public int SubscriptionId { get; set; }
+
+        [Required]
+        public int TownId { get; set; }
+
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string Address { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(20)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(20)]
+        public string LastName { get; set; }
+
+        [Required]
+        [MinLength(5)]
+        [MaxLength(20)]
+        public string Phone { get; set; }
+
+        // TODO: Add reCaptcha
+        public List<SelectListItem> Towns { get; set; }
+
+    }
+}
